017dee0ecdd1f2aeab7e919adc62712617601280
Yeison Suarez <yeison@unlimited-robotics.com>
2025-01-30 17:11:19 -0500

    change links for files

--- services/copy_services.sh
+++ services/copy_services.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 SOURCE_DIR="/opt/ur/services"
@@ -15,11 +14,11 @@ for service_file in "$SOURCE_DIR"/*.service; do
         dest_link="$DEST_DIR/$service_name"
 
         if [[ -e "$dest_link" ]]; then
-            echo "File $dest_link exist."
-        else
-            ln -s "$service_file" "$dest_link"
-            echo "New symbolic link: $service_file -> $dest_link"
+            echo "File $dest_link exist, deleting."
+            rm $dest_link
         fi
+        cp "$service_file" "$dest_link"
+        echo "Copy service: $service_file -> $dest_link"
         systemctl enable "$service_name"
         systemctl start "$service_name"
         echo "Service enabled: $service_name"
